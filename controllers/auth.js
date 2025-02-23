283fa48936f68078a121dad426490b0eb80153b4
akuan1997 <129217093+akuan1997@users.noreply.github.com>
2025-02-24 00:43:19 +0800

    hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,21 @@
 const User = require('../models/user');
+const {StatusCodes} = require('http-status-codes')
+const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-    res.send('register user')
+    const { name, email, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);  // random bytes
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const tempUser = { name, email, password: hashedPassword };
+
+    const user = await User.create({...tempUser});
+
+    // 使用 Mongoose 在 MongoDB 中創建新用戶 (User.create)。
+    // const user = await User.create({...req.body});
+    // 來自 http-status-codes 模組，等同於 201 (表示資源已成功創建)。
+    // .json({ user })：將剛剛創建的用戶物件 user 轉成 JSON 格式並發送回客戶端。
+    res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {
