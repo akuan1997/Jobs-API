2cacf85972402c2ee6f052692a115bf902b95a96
akuan1997 <129217093+akuan1997@users.noreply.github.com>
2025-02-25 20:33:42 +0800

    判斷重複email

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -10,6 +10,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     if (err instanceof CustomAPIError) {
         return res.status(err.statusCode).json({msg: err.message})
     }
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} failed, please choose another value`
+        customError.statusCode = 400
+    }
     // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
     return res.status(customError.statusCode).json({msg: customError.msg })
 }
