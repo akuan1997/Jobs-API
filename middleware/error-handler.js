44481763aa75915a85d715e510e8cf259f698d14
akuan1997 <129217093+akuan1997@users.noreply.github.com>
2025-02-25 20:48:15 +0800

    更改錯誤碼

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,15 +7,21 @@ const errorHandlerMiddleware = (err, req, res, next) => {
         msg: err.message || 'Something went wrong try again later'
     }
 
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({msg: err.message})
+    // if (err instanceof CustomAPIError) {
+    //     return res.status(err.statusCode).json({msg: err.message})
+    // }
+    if (err.name === 'ValidationError') {
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message).join(',')
+        console.log('hello')
+        customError.statusCode = 400
     }
     if (err.code && err.code === 11000) {
         customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} failed, please choose another value`
         customError.statusCode = 400
     }
     // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
-    return res.status(customError.statusCode).json({msg: customError.msg })
+    return res.status(customError.statusCode).json({msg: customError.msg})
 }
 
 module.exports = errorHandlerMiddleware
