d2d79bcfdd8f0ace634666ba8e4fb47fa6a5bcad
akuan1997 <129217093+akuan1997@users.noreply.github.com>
2025-02-25 20:53:01 +0800

    detect CastError and change the statuscode

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -11,6 +11,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     //     return res.status(err.statusCode).json({msg: err.message})
     // }
     if (err.name === 'ValidationError') {
+
         customError.msg = Object.values(err.errors)
             .map((item) => item.message).join(',')
         console.log('hello')
@@ -19,6 +20,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     if (err.code && err.code === 11000) {
         customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} failed, please choose another value`
         customError.statusCode = 400
+    }
+    if(err.name === 'CastError') {
+        customError.msg = `No item found with id: ${err.value}`
+        customError.statusCode = 404
     }
     // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
     return res.status(customError.statusCode).json({msg: customError.msg})
